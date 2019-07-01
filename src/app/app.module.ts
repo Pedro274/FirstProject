8edd4fbb90d6b2ae0e71c16a7e28de257e600dea
Pedro Hernandez <48743143+Pedro274@users.noreply.github.com>
2019-07-01 14:41:42 -0400

    earese warning component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent
   ],
   imports: [
     BrowserModule
